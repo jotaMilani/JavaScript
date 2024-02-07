06a726734f952323922f0bdba0e0ddbceeb209ec
milani <fandangoslord2@gmail.com>
2024-02-07 11:26:32 -0300

    JavaScript basico

---
+++ aritmeticos.js
@@ -0,0 +1,17 @@
+console.log(" a soma entre 8 e 7 é", 8 + 7);
+
+//efetuar a soma de dois números informados por você (var nome variavel = 12)
+
+var x = 2;
+var y = 5;
+console.log("x vale ", x, "y vale ", y, " a soma de x e y é:", x + y);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("soma = ", soma);
+console.log("subtracao = ", subtracao);
+console.log("multiplicacao = ", multiplicacao);
+console.log("divisao = ", divisao);
---
+++ variaveis.js
@@ -0,0 +1,6 @@
+console.log("olá");
+
+var nome = "jota";
+var idade = 15
+console.log("meu nome é ", nome, "e tenho", idade, "anos!");
+console.log("agora fiz aniversário! tenho", idade+1)
